654821efe24447a3caf532b18d05edce6a8ec21a
Andre Figueiredo <afigueiredo@greencents.com>
2016-02-29 12:54:10 -0300

    support for onerror and status property setting

--- spec/test/fakeXMLHTTPRequest.js
+++ spec/test/fakeXMLHTTPRequest.js
@@ -5,14 +5,31 @@ function fakeXMLHTTPRequest() {
   };
   this.onload = function () {
   };
+  this.onerror = function () {
+  };
   this.send = function () {
-    this.onload();
+    if (fakeXMLHTTPRequest.status > 0) {
+      this.onload();
+    }
+    else {
+      this.onerror();
+    }
   };
 
   return this;
 }
+
 fakeXMLHTTPRequest.responseText = '';
+fakeXMLHTTPRequest.status = 200;
 fakeXMLHTTPRequest.withResponse = function(text){
   this.responseText = text;
   return this;
 };
+fakeXMLHTTPRequest.withError = function() {
+  this.status = 0;
+  return this;
+}
+fakeXMLHTTPRequest.withStatus = function(status) {
+  this.status = status;
+  return this;
+}

08d26f7b0f2704dce258d873e530777eda9c062e
Andre Figueiredo <andretf.inf@gmail.com>
2018-06-11 14:30:30 -0400

    chore(tests): update karma and jasmine libraries
    karma and jasmine libraries went broken dependencies preventing from test being ran.

--- karma.conf.js
+++ karma.conf.js
@@ -8,6 +8,16 @@ module.exports = function (config) {
     frameworks: ['jasmine'],
     singleRun: true,
     browsers: ['PhantomJS'],
+    plugins: [
+      'karma-coverage',
+      'karma-jasmine',
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-phantomjs-launcher',
+      'karma-opera-launcher',
+      'karma-ie-launcher',
+      'karma-safari-launcher'
+    ],
     reporters: ['progress', 'coverage'],
     preprocessors: {
       './src/nanofeed.js': ['coverage']

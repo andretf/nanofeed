0446f848b8be796247e6c985b8e5f09c2a1512f9
Andre Figueiredo <andretf.inf@gmail.com>
2018-06-11 22:39:32 -0400

    build(lint): Drop Gulp ES Lint for now.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var gulp = require('gulp'),
-  eslint = require('gulp-eslint'),
   uglify = require('gulp-uglify'),
   rename = require('gulp-rename'),
   size = require('gulp-size'),
@@ -18,19 +17,7 @@ gulp.task('test', function () {
     }));
 });
 
-gulp.task('lint', function () {
-  return gulp.src(source)
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError())
-    .pipe(eslint.results(function (results) {
-      console.log('Total Files: ' + results.length);
-      console.log('Total Warnings: ' + results.warningCount);
-      console.log('Total Errors: ' + results.errorCount);
-    }));
-});
-
-gulp.task('build', ['test', 'lint'], function () {
+gulp.task('build', ['test'], function () {
   return gulp.src(source)
     .pipe(gulp.dest('./dist'))
     .pipe(rename(dest))

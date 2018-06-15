14280f5d7ce0768c07c449e1df0ee965fef33bc4
Andre Figueiredo <andretf.inf@gmail.com>
2018-06-15 00:04:51 -0400

    refactor: a lot of changes to break complexity

--- dist/nanofeed.js
+++ dist/nanofeed.js
@@ -3,121 +3,127 @@
  * (c) 2016-2018 Andre Figueiredo https://github.com/andretf
  * License: MIT
 */
-
-(function () {
-  'use strict';
-
+;
+(function (window) {
+  'use strict'
   if (!window || !window.document) {
-    throw new Error("nanofeed requires a window with a document");
+    throw new Error('nanofeed requires a window with a document')
   }
 
   var defaultOptions = {
     fields: ['title', 'link'],
     qty: 5
-  };
-
-  var query = 'SELECT {COLS} FROM yql.query.multi WHERE queries=\'' +
-    'SELECT {COLS} FROM rss WHERE url in ("{URLS}")' +
-    '|UNIQUE(field="title",hideRepeatCount="true")' +
-    '|UNIQUE(field="link",hideRepeatCount="true")' +
-    '|SORT(field="pubDate",descending="true")' +
-    '|TRUNCATE({QTY})\'';
+  }
 
   var nanofeed = {
     options: defaultOptions,
     fetch: fetch
-  };
+  }
 
-  window.nanofeed = window.nanofeed || nanofeed;
+  window.nanofeed = window.nanofeed || nanofeed
 
   function objToQuerystring(obj) {
-    return encodeURIComponent(Object.keys(obj).map(function (key) {
-      return key + '=' + obj[key]
-    }).join('&'))
+    return encodeURIComponent(
+      Object.keys(obj)
+      .map(function (key) {
+        return key + '=' + obj[key]
+      })
+      .join('&')
+    )
   }
 
   // We don't want extra columns carrying unnecessary data through the network
   function queryColumnNames(fields) {
     if (Array.isArray(fields) && fields.length) {
       fields = ['title', 'link', 'date', 'description'].filter(function (field) {
-        return fields.indexOf(String(field).toLowerCase()) > -1;
-      });
+        return fields.indexOf(String(field).toLowerCase()) > -1
+      })
     } else {
-      fields = defaultOptions.fields;
+      fields = defaultOptions.fields
     }
 
-    return fields.map(function (field) {
-      if (field === 'date') {
-        field = 'pubDate';
-      }
+    return fields
+      .map(function (field) {
+        if (field === 'date') {
+          field = 'pubDate'
+        }
 
-      return 'item.' + field;
-    }).join(',');
+        return 'item.' + field
+      })
+      .join(',')
   }
 
   function getJSON(url, callback) {
-    var request = new XMLHttpRequest();
+    var request = new XMLHttpRequest()
     request.onload = function () {
-      var data;
+      var data
       try {
-        data = JSON.parse(request.responseText);
+        data = JSON.parse(request.responseText)
       } catch (e) {
-        return;
+        return
       }
-      return callback(data);
-    };
-    request.open('GET', url, true);
-    request.send();
+      return callback(data)
+    }
+    request.open('GET', url, true)
+    request.send()
   }
 
-  function onData(options, callback) {
+  function onData(callback) {
     return function (json) {
       if (!json || !json.query || json.query.count === undefined) {
-        return;
+        return
       }
-
-      if (!json.query.count) {
-        return callback([])
-      }
-
-      var data = [];
 
       try {
-        var result = json.query.results.results.item;
-        data = result.length ? result : [result];
+        var result = json.query.results.results.item
+        result = Array.isArray(result) ? result : [result]
+        var data = formatData(result)
+        return callback(data)
       } catch (e) {
-        // ignore error
+        return callback([])
       }
-
-      if (data.length && data[0].pubDate) {
-        data.forEach(function (item) {
-          item.pubDate = new Date(item.pubDate);
-        });
-      }
-
-      return callback(data);
     }
   }
 
   function fetch(urls, options, callback) {
     if (typeof urls === 'string') {
-      urls = [urls];
+      urls = [urls]
     } else if (!Array.isArray(urls) || !urls.length) {
-      return this;
+      return this
     }
 
     if (typeof options === 'function') {
-      callback = options;
-      options = defaultOptions;
+      callback = options
+      options = defaultOptions
     } else {
-      options.qty = isNaN(options.qty) ? defaultOptions.qty : parseInt(options.qty);
+      options.qty = isNaN(options.qty) ?
+        defaultOptions.qty :
+        parseInt(options.qty)
     }
 
-    query = query
+    var query = apiQuery(urls, options)
+    var url = apiUrl(query)
+
+    getJSON(url, onData(callback))
+
+    return this
+  }
+
+  function apiQuery(urls, options) {
+    return (
+      "SELECT {COLS} FROM yql.query.multi WHERE queries='" +
+      'SELECT {COLS} FROM rss WHERE url in ("{URLS}")' +
+      '|UNIQUE(field="title",hideRepeatCount="true")' +
+      '|UNIQUE(field="link",hideRepeatCount="true")' +
+      '|SORT(field="pubDate",descending="true")' +
+      "|TRUNCATE({QTY})'"
       .replace('{URLS}', urls.join('","'))
       .replace('{COLS}', queryColumnNames(options.fields))
-      .replace('{QTY}', options.qty);
+      .replace('{QTY}', options.qty)
+    )
+  }
 
+  function apiUrl(query) {
     // Optimized cross-product make simple array of results in 'query.results.results.item'
     // All tables Env allows select from 'yql.query.multi'
     var yqlQuerystring = objToQuerystring({
@@ -128,9 +134,16 @@
       q: query
     })
 
-    var url = 'https://query.yahooapis.com/v1/public/yql?' + yqlQuerystring;
-
-    getJSON(url, onData(options, callback));
+    return 'https://query.yahooapis.com/v1/public/yql?' + yqlQuerystring
   }
 
-})(window);
+  function formatData(data) {
+    if (data[0].pubDate) {
+      data.forEach(function (item) {
+        item.pubDate = new Date(item.pubDate)
+      })
+    }
+
+    return data
+  }
+})(window)
